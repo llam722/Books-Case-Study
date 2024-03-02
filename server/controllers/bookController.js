c33510ce9d2c85cd1751a1143ba204c7322b58c3
louislam <louislam7229@gmail.com>
2024-03-02 03:44:21 -0800

    chore(controller): fix lint

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -1,37 +1,38 @@
-const { Book } = require("../models/bookModel");
+const { Book } = require('../models/bookModel');
 
 const bookController = {};
 
 //Retrieve a list of all books. Implement pagination to limit the number of books returned per request.
 bookController.getBooks = async (req, res, next) => {
-  // res.status(200).send("This is the books route...");
-  //if a page query parameter is provided, use it to determine which page of books to return, if not, default to the first page
-  const page = parseInt(req.query.page) || 1;
-  //the number of books to return per page
-  const limit = parseInt(req.query.limit) || 10;
-  const skipPage = (page - 1) * limit;
+	// res.status(200).send("This is the books route...");
+	//if a page query parameter is provided, use it to determine which page of books to return, if not, default to the first page
+	const page = parseInt(req.query.page) || 1;
+	//the number of books to return per page
+	const limit = parseInt(req.query.limit) || 10;
+	const skipPage = (page - 1) * limit;
 
-  try {
-    res.locals.books = "books kept here...";
-  } catch (err) {
-    res.status(400).send("Error retrieving books...", err);
-  }
-  return next();
+	try {
+		res.locals.books = 'books kept here...';
+	} catch (err) {
+		res.status(400).send('Error retrieving books...', err);
+	}
+	return next();
 };
 
 bookController.addBook = async (req, res, next) => {
-  try {
-    const { title, author, publicationYear } = req.body;
-    const newBook = await Book.create({
-      title,
-      author,
-      publicationYear,
-    });
-    res.locals.newBook = newBook;
-  } catch (err) {
-    res.status(400).send("Error adding book...", err);
-  }
-  return next();
+	try {
+		const { title, author, publicationYear } = req.body;
+		const newBook = await Book.create({
+			title,
+			author,
+			publicationYear,
+		});
+
+		res.locals.newBook = newBook;
+	} catch (err) {
+		res.status(400).send('Error adding book to database...', err);
+	}
+	return next();
 };
 
 module.exports = bookController;
