163ee3040655683fe07cbe1df303b9cb831f08db
louislam <louislam7229@gmail.com>
2024-03-01 23:48:45 -0800

    feat(mongodb): add mongodb server and create schema for book

---
+++ server/models/mongoDB.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const DB_URI =
+  "mongodb+srv:louislam7229:BcKzUyCXQ2l6wTL9@erewhon.qdpiolq.mongodb.net/?retryWrites=true&w=majority&appName=Erewhon";
+
+mongoose
+  .connect(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "Erewhon",
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const Book = mongoose.model("book", bookSchema);
+
+const bookSchema = new Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  publicationYear: { type: Number, required: true },
+});
+
+module.exports = {
+  Book
+}
