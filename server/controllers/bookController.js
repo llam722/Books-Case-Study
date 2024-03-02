c9935fab58e48e0c9c748838f5895068f4df6ff9
louislam <louislam7229@gmail.com>
2024-03-02 03:31:49 -0800

    feat(controller): set res.locals and test with string

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -4,7 +4,7 @@ const bookController = {};
 
 //Retrieve a list of all books. Implement pagination to limit the number of books returned per request.
 bookController.getBooks = async (req, res, next) => {
-  res.status(200).send("This is the books route...");
+  // res.status(200).send("This is the books route...");
   //if a page query parameter is provided, use it to determine which page of books to return, if not, default to the first page
   const page = parseInt(req.query.page) || 1;
   //the number of books to return per page
@@ -12,12 +12,11 @@ bookController.getBooks = async (req, res, next) => {
   const skipPage = (page - 1) * limit;
 
   try {
-    res.locals.books = 'books';
-    res.status(200).json(books);
+    res.locals.books = "books kept here...";
   } catch (err) {
     res.status(400).send("Error retrieving books...");
   }
   return next();
-}
+};
 
 module.exports = bookController;
