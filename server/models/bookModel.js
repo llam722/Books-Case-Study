9375a71747d436bca86b22da08b398e5c0f043ed
louislam <louislam7229@gmail.com>
2024-03-03 23:03:49 -0800

    feat(bookModel): update bookModel with comments

--- server/models/bookModel.js
+++ server/models/bookModel.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 
-
 const Schema = mongoose.Schema;
 
+//mongodb URI for connecting to the database, hardcoded for now, but should be stored in an environment variable (.env) for security
 const DB_URI =
   "mongodb+srv://louislam7229:weyGIDgLqyE1CIDc@erewhon.qdpiolq.mongodb.net/?retryWrites=true&w=majority&appName=Erewhon";
 
+//connect to the database
 mongoose
   .connect(DB_URI, {
     dbName: "Erewhon",
@@ -17,7 +18,7 @@ mongoose
     console.log(err, 'Connection to MongoDB failed...');
   });
 
-  
+  //create a new schema for the book model
   const bookSchema = new Schema({
     title: { type: String, required: true },
     author: { type: String, required: true },
