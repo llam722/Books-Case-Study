e9d40b7fa20196c8164ce2ccb972533f7cfc2fb6
louislam <louislam7229@gmail.com>
2024-03-03 19:57:40 -0800

    feat(validation): add validation to post request in router

--- server/routes/bookRouter.js
+++ server/routes/bookRouter.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { body, validationResult } from 'express-validator';
 import bookController from '../controllers/bookController.js';
 
 const router = express.Router();
@@ -24,9 +25,32 @@ router.get('/:id', bookController.getBookById, (req, res) => {
 });
 
 //post request to add a new book to the collection
-router.post('/', bookController.addBook, (req, res) => {
-	res.status(201).json(res.locals.newBook);
-});
+router.post(
+	'/',
+	//validate the request body
+	[
+		//trims white space, ensures field is populated and escapes special characters
+		body('title').trim().notEmpty().withMessage('Title is required').escape(),
+		body('author').trim().notEmpty().withMessage('Author is required').escape(),
+		body('publicationYear')
+			.isInt({ min: 1, max: new Date().getFullYear() }) //validates publication year
+			.withMessage('Publication year is required and must be a valid year') //error message if invalid
+			.toInt(), //converts the value to an integer
+	],
+	(req, res, next) => {
+		//check for validation errors
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		return next();
+	},
+
+	bookController.addBook,
+	(req, res) => {
+		res.status(201).json(res.locals.newBook);
+	}
+);
 
 //put request to update a specific book by ID
 router.put('/:id', bookController.updateBook, (req, res) => {
