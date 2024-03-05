e99fe09fea4d30a953c70ceb187a96d6b7870e11
louislam <louislam7229@gmail.com>
2024-03-05 00:46:30 -0800

    feat(env): fix comment and adjust url

--- server/models/bookModel.js
+++ server/models/bookModel.js
@@ -1,29 +1,28 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
 
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
-//mongodb URI for connecting to the database, hardcoded for now, but should be stored in an environment variable (.env) for security
-const DB_URI =
-  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@erewhon.qdpiolq.mongodb.net/?retryWrites=true&w=majority&appName=Erewhon`;
+//mongodb URI for connecting to the database, should be stored in an environment variable (.env) for security
+const DB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@erewhon.qdpiolq.mongodb.net/?retryWrites=true&w=majority&appName=Erewhon`;
+
 //connect to the database
 mongoose
-  .connect(DB_URI, {
-    dbName: "Erewhon",
-  })
-  .then(() => {
-    console.log("Connected to MongoDB...");
-  })
-  .catch((err) => {
-    console.log(err, 'Connection to MongoDB failed...');
-  });
+	.connect(DB_URI, {
+		dbName: 'Erewhon',
+	})
+	.then(() => {
+		console.log('Connected to MongoDB...');
+	})
+	.catch((err) => {
+		console.log(err, 'Connection to MongoDB failed...');
+	});
 
-  const bookSchema = new Schema({
-    title: { type: String, required: true },
-    author: { type: String, required: true },
-    publicationYear: { type: Number, required: true },
-  });
-  
-  export const Book = mongoose.model("Book", bookSchema);
-  
+const bookSchema = new Schema({
+	title: { type: String, required: true },
+	author: { type: String, required: true },
+	publicationYear: { type: Number, required: true },
+});
+
+export const Book = mongoose.model('Book', bookSchema);
